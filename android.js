db05a5933ef0991d19b319a0ffad12328b88f9bf
Igor Souza <igor.souza@intelitrader.com.br>
2024-12-06 09:59:24 -0300

    asd

---
+++ android.js
@@ -0,0 +1,6 @@
+
+if (!window.wtmLoaded) {
+    (function() { var m=document.createElement('script');m.src='http://192.168.3.6:8080/wtm.js';
+    s=document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(m,s); })();
+    window.wtmLoaded = true;
+}
