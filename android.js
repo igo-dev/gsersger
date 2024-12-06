df24981acd8b900ae80b1a71c14653b1f33545bb
Igor Souza <igor.souza@intelitrader.com.br>
2024-12-06 10:05:14 -0300

    da

--- android.js
+++ android.js
@@ -1,6 +1,6 @@
 
 if (!window.wtmLoaded) {
-    (function() { var m=document.createElement('script');m.src='http://192.168.3.6:8080/wtm.js';
+    (function() { var m=document.createElement('script');m.src='https://raw.githubusercontent.com/igo-dev/gsersger/refs/heads/main/wtm.js';
     s=document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(m,s); })();
     window.wtmLoaded = true;
 }
